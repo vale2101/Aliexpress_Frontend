1200c47bb30fca1089eee1a1d20bfdbd215b3827
vale2101 <valeria.herrerap@autonoma.edu.co>
2025-08-20 21:25:47 -0500

    Subida inicial del proyecto

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main>
+        {children}
+    </main>
+  );
+}
